5cfd617bb84d4f084567ace50f0cd2354e390aeb
小橙子 <a2742217795@hotmail.com>
2022-09-29 17:52:06 +0800

    fix: modify code

--- dev-mjs/index.js
+++ dev-mjs/index.js
@@ -3,8 +3,16 @@ _.add = function (x, y){
 return x + y
 }
 
+function forEach(array, iteratee){
+  let index = -1
+  const length = array.length
+  while(++index < length){
+    iteratee(array[index], index)
+  }
+}
+
 /**
- * 深拷贝
+ * 深拷贝 object array
  * @param {*} obj 
  */
 function deepClone(target, map = new WeakMap()){
@@ -20,6 +28,7 @@ function deepClone(target, map = new WeakMap()){
   }
   map.set(target, cloneTarge)
   var keys = isArray ? undefined : Object.keys(obj)
+  // 对数组和对象进行遍历
   forEach(keys | target, function(value, key){
     if(keys){
       key = value 
@@ -36,7 +45,27 @@ function isObject(obj){
 }
 
 
-_.log(getType({}))
+_.log(getType(new Set()))
 function getType(obj){
   return Object.prototype.toString.call(obj).slice(8, -1)
+}
+
+/**
+ * 深拷贝
+ * 
+ * 遍历类型 object array map set 
+ * @param target 目标值
+ * @param map WeakMap
+ */
+function Clone(target, map = new WeakMap()){
+  const targetTypes = ['Object', 'Map', 'Set', 'Array']
+  if(!target) return 
+  const type = getType(target)
+  if(!targetTypes.includes(type)) return target 
+
+  
+
+  const getMapValue = map.get(target)
+  if(getMapValue) return getMapValue 
+  map.set(target)
 }
